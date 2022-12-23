148e4027bafccb09ed80fb3dd2276c7dc3b569d7
Shakir Farhan <shakirfarhan123@gmail.com>
2022-12-23 10:32:40 +0530

    console

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const port = 8000;
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+console.log("edam");
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
